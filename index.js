0541985440fdce25441d365bb3210449cff262f3
Henry <u20201c579@upc.edu.pe>
2024-02-26 16:24:55 -0500

    books pdf

--- index.js
+++ index.js
@@ -1,65 +1,83 @@
-const sideLinks = document.querySelectorAll('.sidebar .side-menu li a:not(.logout)');
+const sideLinks = document.querySelectorAll(
+  ".sidebar .side-menu li a:not(.logout)"
+);
 
-sideLinks.forEach(item => {
-    const li = item.parentElement;
-    item.addEventListener('click', () => {
-        sideLinks.forEach(i => {
-            i.parentElement.classList.remove('active');
-        })
-        li.classList.add('active');
-    })
+sideLinks.forEach((item) => {
+  const li = item.parentElement;
+  item.addEventListener("click", () => {
+    sideLinks.forEach((i) => {
+      i.parentElement.classList.remove("active");
+    });
+    li.classList.add("active");
+  });
 });
 
-const menuBar = document.querySelector('.content nav .bx.bx-menu');
-const sideBar = document.querySelector('.sidebar');
+const menuBar = document.querySelector(".content nav .bx.bx-menu");
+const sideBar = document.querySelector(".sidebar");
 
-menuBar.addEventListener('click', () => {
-    sideBar.classList.toggle('close');
+menuBar.addEventListener("click", () => {
+  sideBar.classList.toggle("close");
 });
 
-const searchBtn = document.querySelector('.content nav form .form-input button');
-const searchBtnIcon = document.querySelector('.content nav form .form-input button .bx');
-const searchForm = document.querySelector('.content nav form');
+const searchBtn = document.querySelector(
+  ".content nav form .form-input button"
+);
+const searchBtnIcon = document.querySelector(
+  ".content nav form .form-input button .bx"
+);
+const searchForm = document.querySelector(".content nav form");
 
-searchBtn.addEventListener('click', function (e) {
-    if (window.innerWidth < 576) {
-        e.preventDefault;
-        searchForm.classList.toggle('show');
-        if (searchForm.classList.contains('show')) {
-            searchBtnIcon.classList.replace('bx-search', 'bx-x');
-        } else {
-            searchBtnIcon.classList.replace('bx-x', 'bx-search');
-        }
-    }
-});
-
-window.addEventListener('resize', () => {
-    if (window.innerWidth < 768) {
-        sideBar.classList.add('close');
+searchBtn.addEventListener("click", function (e) {
+  if (window.innerWidth < 576) {
+    e.preventDefault;
+    searchForm.classList.toggle("show");
+    if (searchForm.classList.contains("show")) {
+      searchBtnIcon.classList.replace("bx-search", "bx-x");
     } else {
-        sideBar.classList.remove('close');
+      searchBtnIcon.classList.replace("bx-x", "bx-search");
     }
-    if (window.innerWidth > 576) {
-        searchBtnIcon.classList.replace('bx-x', 'bx-search');
-        searchForm.classList.remove('show');
-    }
+  }
 });
 
-const toggler = document.getElementById('theme-toggle');
+window.addEventListener("resize", () => {
+  if (window.innerWidth < 768) {
+    sideBar.classList.add("close");
+  } else {
+    sideBar.classList.remove("close");
+  }
+  if (window.innerWidth > 576) {
+    searchBtnIcon.classList.replace("bx-x", "bx-search");
+    searchForm.classList.remove("show");
+  }
+});
 
-toggler.addEventListener('change', function () {
-    if (this.checked) {
-        document.body.classList.add('dark');
-    } else {
-        document.body.classList.remove('dark');
-    }
+const toggler = document.getElementById("theme-toggle");
+
+toggler.addEventListener("change", function () {
+  if (this.checked) {
+    document.body.classList.add("dark");
+  } else {
+    document.body.classList.remove("dark");
+  }
 });
 
 function toggleTables(event) {
-    var clickedOrders = event.currentTarget.parentElement;
-    var tables = clickedOrders.querySelectorAll('.table-container');
+  var clickedOrders = event.currentTarget.parentElement;
+  var tables = clickedOrders.querySelectorAll(".table-container");
 
-    tables.forEach(function(table) {
-        table.style.display = (table.style.display === "block") ? "none" : "block";
-    });
+  tables.forEach(function (table) {
+    table.style.display = table.style.display === "block" ? "none" : "block";
+  });
+}
+
+function mostrarPDF(pdfSrc) {
+  // Obtener el contenedor del PDF y el visor de PDF
+  var pdfContainer = document.getElementById("pdfContainer");
+  var pdfViewer = document.getElementById("pdfViewer");
+
+  // Establecer la fuente del PDF
+  pdfViewer.src = pdfSrc;
+
+  // Mostrar el contenedor del PDF
+  pdfContainer.style.display = "block";
 }
